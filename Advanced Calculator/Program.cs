ff9875f221604ddcd8b95a9e228cba3dc346184d
Bogdan Gheorghe <dodebogdan@yahoo.com>
2020-12-10 11:00:21 +0200

    Add project files.

---
+++ Advanced Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advanced_Calculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
